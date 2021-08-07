7e8ca9dff9df3c27c9a85bf13eeb7de29a4d45e0
Amnesia <77104443+CuSO4-c3c@users.noreply.github.com>
2021-08-07 08:03:53 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.facebook.com/hellsnake98/";
+            window.location = "https://www.facebook.com/profile.php?id=100066295651777";
           },
         });
       }
